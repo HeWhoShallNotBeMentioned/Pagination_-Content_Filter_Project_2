91777b7f27270542696336a6258b08bb632d6923
Chris Underwood <cunderwoodmn@gmail.com>
2016-10-30 00:59:18 -0700

    able to grab records from the list

---
+++ js/app.js
@@ -0,0 +1,37 @@
+//create pagination
+  //loop through elements to count them
+  //queryselectorall? li.student-item
+  // var parent = document.getElementById('parent');
+  // var child_nodes = parent.childNodes;
+  // console.log(child_nodes.length); // let's assume "2"
+  // parent.appendChild(document.createElement('div'));
+  // console.log(child_nodes.length); // should output "3"
+  //https://developer.mozilla.org/en-US/docs/Web/API/NodeList
+
+  var students = document.querySelectorAll('li.student-item');
+//console.log(students);
+console.log(students.length);
+//console.log(students[0]);//gives us the html elements that we need for items
+//pageNum * 10 - 1 ; the loop start; loop end would be initial start + 10
+var pageHtml = " ";
+var slidesHolder = document.getElementsByClassName("student-list");
+console.log(slidesHolder);
+
+for (var i = 0; i < 3; i++) {
+  //pageHtml += students[i] + " ";
+    slidesHolder.appendChild(students[i]);
+  //console.log(i, pageHtml);
+}
+
+
+
+console.log(pageHtml);
+slidesHolder.innerHTML();
+
+
+  //number of pages: var div = Math.floor(total# of items/10);
+  //number of items on the last page: var rem = total# of items % 10;
+
+
+
+//search give current items on each click up
