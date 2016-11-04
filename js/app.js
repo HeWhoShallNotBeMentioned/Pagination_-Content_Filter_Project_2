7bf3b9cd7f48e4d2f1fd6fef2abde47b2bb0fc24
Chris Underwood <cunderwoodmn@gmail.com>
2016-11-04 14:27:26 -0700

    starting js over

--- js/app.js
+++ js/app.js
@@ -8,26 +8,28 @@
   // console.log(child_nodes.length); // should output "3"
   //https://developer.mozilla.org/en-US/docs/Web/API/NodeList
 
-  var students = document.querySelectorAll('li.student-item');
+  //var students = document.querySelectorAll('li.student-item');
 //console.log(students);
-console.log(students.length);
+//console.log(students.length);
 //console.log(students[0]);//gives us the html elements that we need for items
 //pageNum * 10 - 1 ; the loop start; loop end would be initial start + 10
-var pageHtml = " ";
-var slidesHolder = document.getElementsByClassName("student-list");
-console.log(slidesHolder);
+//var pageHtml = " ";
+//var slidesHolder = document.getElementsByClassName("student-list");
+//onsole.log(slidesHolder);
 
-for (var i = 0; i < 3; i++) {
+//for (var i = 0; i < 3; i++) {
   //pageHtml += students[i] + " ";
-    slidesHolder.appendChild(students[i]);
+//    slidesHolder.appendChild(students[i]);
   //console.log(i, pageHtml);
-}
+//}
 
 
 
-console.log(pageHtml);
-slidesHolder.innerHTML();
+//console.log(pageHtml);
+//slidesHolder.innerHTML();
 
+//looping over a nodelist
+//http://www.w3schools.com/js/js_htmldom_nodelist.asp
 
   //number of pages: var div = Math.floor(total# of items/10);
   //number of items on the last page: var rem = total# of items % 10;
