07d195f86b204507b65e39149369804af65a1df3
Chris Underwood <cunderwoodmn@gmail.com>
2016-11-04 21:08:17 -0700

    Able to hide the original list and put any items back on the page.

--- js/app.js
+++ js/app.js
@@ -8,20 +8,16 @@
   // console.log(child_nodes.length); // should output "3"
   //https://developer.mozilla.org/en-US/docs/Web/API/NodeList
 
-  //var students = document.querySelectorAll('li.student-item');
-//console.log(students);
-//console.log(students.length);
+  var students = document.querySelectorAll('li.student-item');
+console.log("Students list:  ", students);
+console.log("Number of students:  ", students.length);
 //console.log(students[0]);//gives us the html elements that we need for items
 //pageNum * 10 - 1 ; the loop start; loop end would be initial start + 10
 //var pageHtml = " ";
 //var slidesHolder = document.getElementsByClassName("student-list");
 //onsole.log(slidesHolder);
 
-//for (var i = 0; i < 3; i++) {
-  //pageHtml += students[i] + " ";
-//    slidesHolder.appendChild(students[i]);
-  //console.log(i, pageHtml);
-//}
+
 
 
 
@@ -33,7 +29,19 @@
 
   //number of pages: var div = Math.floor(total# of items/10);
   //number of items on the last page: var rem = total# of items % 10;
-
-
+var pageNum = Math.floor((students.length/10) + 1);
+console.log("page #s:   ", pageNum);
+var students = document.querySelectorAll('li.student-item');
 
 //search give current items on each click up
+console.log(document.getElementsByClassName('student-list'));
+//console.log(document.getElementsByClassName('student-list')[0].children);
+//document.getElementsByClassName('student-list')[0].children[6].style.display = 'none';
+
+for (var i = 0; i < students.length; i++) {
+  document.getElementsByClassName('student-list')[0].children[i].style.display = 'none';
+}
+
+for (var i = 19; i < 29; i++) {
+  document.getElementsByClassName('student-list')[0].children[i].style.display = 'block';
+}
